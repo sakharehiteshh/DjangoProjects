68f9bb2345365deeda09bf5521c4a7d8f32a38ef
Hitesh Vilas Sakhare <sakhareh1@udayton.edu>
2022-12-15 06:05:44 -0500

    Project part done

--- Bookmarks/bookmarks/account/models.py
+++ Bookmarks/bookmarks/account/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.conf import settings
+from django.contrib.auth import get_user_model
 
 
 class Profile(models.Model):
@@ -18,3 +19,27 @@ def save_profile(backend, user, response, *args, **kwargs):
 		except Profile.DoesNotExist:
 			profile = Profile(user = user)
 			profile.save()
+
+
+class Contact(models.Model):
+	user_from = models.ForeignKey('auth.User',
+				related_name='rel_from_set',
+				on_delete=models.CASCADE)
+	user_to = models.ForeignKey('auth.User',
+				related_name='rel_to_set',
+				on_delete=models.CASCADE)
+	created = models.DateTimeField(auto_now_add=True,
+			db_index=True)
+
+	class Meta:
+		ordering = ('-created',)
+
+	def __str__(self):
+		return f"{self.user_from} follows {self.user_to}"
+
+user_model = get_user_model()
+user_model.add_to_class('following',
+						models.ManyToManyField('self',
+												through=Contact,
+												related_name='followers',
+												symmetrical=False))
